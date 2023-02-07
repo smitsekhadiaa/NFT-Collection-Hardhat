27aaac7db5e0cad9e0811bc1e7815ebb3ea97afe
Smit <smitsekhadia99@gmail.com>
2023-02-07 11:26:02 +0530

    smart contract deployed

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x65920041d5924D758A7a1367F6044707cC527Ca4";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config({ path: ".env" });
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const GOERLI_PVT_KEY = process.env.GOERLI_PVT_KEY;
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [GOERLI_PVT_KEY],
+    },
+  },
 };
